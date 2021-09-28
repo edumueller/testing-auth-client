10d5fc2af94b7fd38c5f60c6f633d62256595703
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-09-28 19:17:30 -0300

    display err and redirect after sign up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,17 @@
 import axios from "axios";
-import { AUTH_USER } from "./types";
+import { AUTH_USER, AUTH_ERROR } from "./types";
 
-export const signup = (formProps) => {
-  return (dispatch) => {
-    axios.post("http://localhost:3090/signup", formProps);
-    // dispatch({ type: AUTH_USER });
+export const signup = (formProps, callback) => {
+  return async (dispatch) => {
+    try {
+      const { token } = await axios.post(
+        "http://localhost:3090/signup",
+        formProps
+      );
+      dispatch({ type: AUTH_USER, payload: token });
+      callback();
+    } catch (e) {
+      dispatch({ type: AUTH_ERROR, payload: "Email in use" });
+    }
   };
 };
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -5,7 +5,9 @@ import { connect } from "react-redux";
 import * as actions from "../../actions";
 class Signup extends Component {
   onSubmit = (formProps) => {
-    this.props.signup(formProps);
+    this.props.signup(formProps, () => {
+      this.props.history.push("/feature");
+    });
   };
 
   render() {
@@ -30,13 +32,18 @@ class Signup extends Component {
             autoComplete="none"
           />
         </fieldset>
+        <div>{this.props.errorMessage}</div>
         <button>Sign Up!</button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
 export default compose(
-  connect(null, actions),
+  connect(mapStateToProps, actions),
   reduxForm({ form: "signup" })
 )(Signup);

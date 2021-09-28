237a68bef907c981cdc797c7da225d5a262129a2
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-09-28 19:24:39 -0300

    fix redirection

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,28 +1,26 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import CommentBox from "./CommentBox";
-
-const requireAuth = (ChildComponent) => {
+export default (ChildComponent) => {
   class ComposedComponent extends Component {
+    // Our component just got rendered
     componentDidMount() {
       this.shouldNavigateAway();
     }
+    // Our component just got updated
     componentDidUpdate() {
       this.shouldNavigateAway();
     }
     shouldNavigateAway() {
-      if (!this.props?.auth) this.props.history.push("/");
+      if (!this.props.auth) {
+        this.props.history.push("/");
+      }
     }
     render() {
       return <ChildComponent {...this.props} />;
     }
   }
-
   function mapStateToProps(state) {
-    return { auth: state.auth.authenticated };
+    return { auth: state.auth };
   }
-
   return connect(mapStateToProps)(ComposedComponent);
 };
-
-export default requireAuth(CommentBox);
